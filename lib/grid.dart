bc77d1ba6d91b950fc1eefb0273f9a3973a54190
Jose Manuel Márquez <markezsoft@gmail.com>
2020-01-10 11:31:26 +0100

    Add callbacks to GridButton for fix error calculating the button pressed from the Grid

--- lib/grid.dart
+++ lib/grid.dart
@@ -6,6 +6,7 @@ import 'package:flutternome/grid_state.dart';
 import 'package:flutternome/grid_button.dart';
 
 class Grid extends StatelessWidget {
+
   final int gridSize;
 
   const Grid({Key key, this.gridSize}) : super(key: key);
@@ -16,7 +17,18 @@ class Grid extends StatelessWidget {
       gridSize,
       (columnIndex) => List.generate(
         gridSize,
-        (rowIndex) => GridButton(rowIndex, columnIndex),
+        (rowIndex) => GridButton(
+          rowIndex,
+          columnIndex,
+          onTapDown: (details) {
+            Provider.of<GridState>(context).isButtonSelected(columnIndex, rowIndex)
+              ? Provider.of<GridState>(context).removeButton(columnIndex, rowIndex)
+              : Provider.of<GridState>(context).addButton(columnIndex, rowIndex);
+          },
+          onPanUpdate: (details) {
+            Provider.of<GridState>(context).addButton(columnIndex, rowIndex);
+          }
+        ),
       ),
     );
 
@@ -34,30 +46,14 @@ class Grid extends StatelessWidget {
           .toList(),
     );
 
-    return GestureDetector(
-      onTapDown: (details) {
-        int column = (details.localPosition.dx / GridSize.divisionWidth).floor();
-        int row = (details.localPosition.dy / GridSize.divisionHeight).floor();
-
-        Provider.of<GridState>(context).isButtonSelected(column, row)
-            ? Provider.of<GridState>(context).removeButton(column, row)
-            : Provider.of<GridState>(context).addButton(column, row);
-      },
-      onPanUpdate: (details) {
-        int column = (details.localPosition.dx / GridSize.divisionWidth).floor();
-        int row = (details.localPosition.dy / GridSize.divisionHeight).floor();
-
-        Provider.of<GridState>(context).addButton(column, row);
-      },
-      child: Container(
-        width: GridSize.width,
-        height: GridSize.height,
-        decoration: BoxDecoration(
-            color: Colors.grey[200],
-            borderRadius: BorderRadius.circular(5.0),
-            border: Border.all(width: 2.0, color: Color(0xFF3e3e3e))),
-        child: buttonGrid,
-      ),
+    return Container(
+      width: GridSize.width,
+      height: GridSize.height,
+      decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(5.0),
+          border: Border.all(width: 2.0, color: Color(0xFF3e3e3e))),
+      child: buttonGrid,
     );
   }
 }
